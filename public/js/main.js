9f8a67f34c477ac33f227f1f673093abd6b777de
TheFrenchGhosty <TheFrenchGhosty@posteo.is>
2022-05-29 14:51:23 +0200

    Unminify the JavaScript file, and remove everything that isn't used

---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+(() => {
+  document.getElementById('mode').addEventListener('click', () => {
+    document.body.classList.toggle('dark'),
+      localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light')
+  }),
+    localStorage.getItem('theme') === 'dark' && document.body.classList.add('dark')
+})()
